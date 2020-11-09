b142d2aba5704eb78c59a1e137ceb117a26a8bfa
sagargithub7 <74170021+sagargithub7@users.noreply.github.com>
2020-11-09 22:35:52 +0530

    Add files via upload

---
+++ Greater Number.py
@@ -0,0 +1,8 @@
+x = int(input("Enter any number\n"))
+
+if x==0:
+    print("Please enter non zero number")
+elif x>0:
+    print("Given Number is Positive Number")
+else:
+    print("Given Number is Negative Number")
